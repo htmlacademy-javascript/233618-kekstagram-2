8f54940da9fac82be6555d35b39cb2fb7d9eea2b
Vadim Golubev <golubevvadik2013@yandex.ru>
2026-05-14 14:18:03 +0300

    Добавляет функционал загрузки изображения, валидацию формы загрузки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { genereteData } from './data.js';
 import { renderGalley } from './components/gallery.js';
 import { renderPhotoModal } from './components/photo-modal.js';
+import { renderUploadModal } from './components/upload-modal.js';
 
 const data = genereteData();
 const picturesElement = document.querySelector('.pictures');
 
 renderGalley(data, picturesElement);
 renderPhotoModal(data, picturesElement);
+renderUploadModal();
