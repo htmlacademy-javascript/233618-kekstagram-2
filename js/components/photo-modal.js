c28f8a729c86ff8697b6f7cf132c827b601a4991
Vadim Golubev <golubevvadik2013@yandex.ru>
2026-05-15 16:23:45 +0300

    Revert "Рефакторинг"
    This reverts commit eef7c4b575f97e4f9063c715e70da7744d077a0a.

--- js/components/photo-modal.js
+++ js/components/photo-modal.js
@@ -69,11 +69,8 @@ const renderPhotoModal = (data, toggleElement) => {
   toggleElement.addEventListener('click', (evt) => openPhotoModal(evt, data));
   commentsLoaderElement.addEventListener('click', onLoaderClick);
 
-  document.addEventListener('keydown', (evt) => {
-    if (
-      evt.target.matches('.picture') &&
-      (isEnterKey(evt) || isSpaceKey(evt))
-    ) {
+  toggleElement.addEventListener('keydown', (evt) => {
+    if (isEnterKey(evt) || isSpaceKey(evt)) {
       openPhotoModal(evt, data);
     }
   });
